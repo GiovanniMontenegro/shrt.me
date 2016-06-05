b1f34576dd8019a32dc15bc49c6f9f53e0fda96f
Giovanni Montenegro <giovanni.montenegroit@gmail.com>
2016-06-05 18:09:44 +0200

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name shrtmeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the shrtmeApp
+ */
+angular.module('shrtmeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name shrtmeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the shrtmeApp
+ */
+angular.module('shrtmeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
